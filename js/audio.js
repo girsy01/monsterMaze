30c9d9818b3e1649312c5b315b69ff9a8f147832
Franziska Scheer <franzi-scheer@web.de>
2024-10-17 21:40:55 +0200

    adjust volume of sound effects

--- js/audio.js
+++ js/audio.js
@@ -1,19 +1,26 @@
 class MyAudio {
   constructor() {
+    this.volumeSoundEffects = 0.8;
+    this.volumeMusic = 0.5;
     this.gameStarted = false;
     this.musicOn = true;
     this.audioOn = true;
     this.musicButtonElement = document.getElementById("music-btn");
     this.soundEffectsButtonElement = document.getElementById("soundEffects-btn");
     this.soundBackgroundMusic = new Audio("sounds/background.mp3");
+    this.soundBackgroundMusic.volume = this.volumeMusic;
     this.soundBackgroundMusic.loop = true;
-    this.soundBackgroundMusic.volume = 0.5;
     this.soundLife = new Audio("sounds/life.mp3");
+    this.soundLife.volume = this.volumeSoundEffects;
     this.soundCoin = new Audio("sounds/coin.mp3");
+    this.soundCoin.volume = this.volumeSoundEffects;
     this.soundCoin.playbackRate = 2; // 1.5x faster
     this.soundLevel = new Audio("sounds/level.wav");
+    this.soundLevel.volume = this.volumeSoundEffects;
     this.soundCollision = new Audio("sounds/collision.wav");
+    this.soundCollision.volume = this.volumeSoundEffects;
     this.soundGameover = new Audio("sounds/gameover.wav");
+    this.soundGameover = this.volumeSoundEffects;
     this.soundGameover.playbackRate = 1.2; // 1.5x faster
   }
 
